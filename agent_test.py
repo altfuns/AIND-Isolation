3b511e403896c7c7323dbc0281c33b97d2bf500c
Alfonso Aguilar <laltfuns@gmail.com>
2017-05-19 14:15:37 -0600

    Add dummy unit test to play the games with AB custom scores

--- agent_test.py
+++ agent_test.py
@@ -19,9 +19,10 @@ class IsolationTest(unittest.TestCase):
 
     def setUp(self):
         reload(game_agent)
-        self.player1 = "Player1"
-        self.player2 = "Player2"
-        self.game = isolation.Board(self.player1, self.player2)
+        self.AB_custom_score = AlphaBetaPlayer(score_fn=custom_score)
+        self.AB_custom_score_2 = AlphaBetaPlayer(score_fn=custom_score_2)
+        self.AB_custom_score_3 = AlphaBetaPlayer(score_fn=custom_score_3)
+        self.AB_Improved = AlphaBetaPlayer(score_fn=improved_score)
 
     def test_minimax(self):
         self.player1 = MinimaxPlayer(timeout=0.)
@@ -31,15 +32,33 @@ class IsolationTest(unittest.TestCase):
         print("\nWinner: {}\nOutcome: {}".format(winner, outcome))
         print(self.game.to_string())
         print("Move history:\n{!s}".format(history))
-        
-    def test_alphabeta(self):
-        self.player1 = AlphaBetaPlayer(timeout=0.)
-        self.player2 = AlphaBetaPlayer(timeout=0.)
-        self.game = isolation.Board(self.player1, self.player2)
-        winner, history, outcome = self.game.play()
-        print("\nWinner: {}\nOutcome: {}".format(winner, outcome))
-        print(self.game.to_string())
+
+    def test_alphabeta_custom_score(self):
+        print("AB_custom_score as Player 1")
+        self.play(self.AB_custom_score, self.AB_Improved)
+        print("AB_custom_score as Player 2")
+        self.play(self.AB_Improved, self.AB_custom_score)
+
+    def test_alphabeta_custom_score_2(self):
+        print("AB_custom_score2 as Player 1")
+        self.play(self.AB_custom_score_2, self.AB_Improved)
+        print("AB_custom_score2 as Player 2")
+        self.play(self.AB_Improved, self.AB_custom_score_2)
+
+    def test_alphabeta_custom_score_3(self):
+        print("AB_custom_score3 as Player 1")
+        self.play(self.AB_custom_score_2, self.AB_Improved)
+        print("AB_custom_score3 as Player 2")
+        self.play(self.AB_Improved, self.AB_custom_score_2)
+
+    def play(self, player_1, player_2):
+        game = isolation.Board(player_1, player_2)
+        winner, history, outcome = game.play()
+        print("\nWinner: {}\nOutcome: {}".format('Player 1' if winner == player_1 else 'Player 2', outcome))
+        print(game.to_string())
         print("Move history:\n{!s}".format(history))
+
+
 
 
 
